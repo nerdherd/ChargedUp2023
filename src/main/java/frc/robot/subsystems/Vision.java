d5f81ea99c44cdcafcac81fa3c59f3150c4bf4ab
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2023-01-09 14:51:07 -0800

    Add vision subsystem and vendordep

---
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase{
+    private PhotonCamera photonCamera; // protect proton camera
+    private PhotonTrackedTarget photonTrackedTarget;
+
+    public boolean limelightHasTargets;
+
+    public Vision(){
+        photonCamera = new PhotonCamera("687Limelight1");
+    }
+
+    @Override
+    public void periodic() {
+        var result = photonCamera.getLatestResult();
+        limelightHasTargets = result.hasTargets();
+        SmartDashboard.putBoolean("Target Present", limelightHasTargets);
+
+        if (limelightHasTargets) { 
+            photonTrackedTarget = result.getBestTarget();
+            logToSmartDashboard();
+        } 
+    }
+
+    public double getPitch() {
+        return photonTrackedTarget.getPitch();
+    }
+
+    public double getYaw() {
+        return photonTrackedTarget.getYaw();
+    }
+
+    public int getFiducialId() {
+        return photonTrackedTarget.getFiducialId();
+    }
+
+    private void logToSmartDashboard() {
+        SmartDashboard.putNumber("Yaw", getYaw());
+        SmartDashboard.putNumber("ID", getFiducialId());
+        SmartDashboard.putNumber("Pitch", getPitch());
+    }
+
+}
