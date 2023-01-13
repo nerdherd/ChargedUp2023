7c403dcf63af6fd62436eb8323f70aa5070da9d0
Kaden B <Tategami99@users.noreply.github.com>
2023-01-13 14:41:54 -0800

    Reset encoders every 10 seconds
    Co-authored-by: londonwafflez@users.noreply.github.com
    Co-authored-by: brokenfloppydisk@users.noreply.github.com

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -78,6 +78,8 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+
+    m_robotContainer.swerveCommand.schedule();
   }
 
   /** This function is called periodically during operator control. */
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,7 +13,10 @@ import frc.robot.subsystems.Vision;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RepeatCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.PS4Controller;
@@ -54,6 +57,12 @@ public class RobotContainer {
   // Two different drivetrain modes
   private RunCommand arcadeRunCommand = new RunCommand(() -> drive.tankDrive(driverController.getLeftY(), driverController.getRightY()), drive);
   private RunCommand visionRunCommand = new RunCommand(() -> drive.arcadeDrive(drive.getApriltagLinear(), drive.getApriltagRotation()), drive);
+
+  public Command swerveCommand = new RepeatCommand(
+    new SequentialCommandGroup(
+        new WaitCommand(10),
+        new InstantCommand(swerveDrive::resetEncoders)
+    ));
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
