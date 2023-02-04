039937e06bed6d35367a5bc0f94e96c0dfc809aa
Kyle Huang <brokenfloppydisk@users.noreply.github.com>
2023-02-04 15:48:56 -0800

    Separate accel and decel limits on drive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -194,6 +194,9 @@ public final class Constants {
     public static final double kPhysicalMaxSpeedMetersPerSecond = 5;    
     public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 2 * 2 * Math.PI;
 
+    public static final double kTeleMaxAcceleration = 3;
+    public static final double kTeleMaxDeceleration = 2;
+
     public static final double kTeleDriveMaxSpeedMetersPerSecond = kPhysicalMaxSpeedMetersPerSecond / 2;
     public static final double kTeleDriveMaxAngularSpeedRadiansPerSecond = //
       kPhysicalMaxAngularSpeedRadiansPerSecond / 2;
