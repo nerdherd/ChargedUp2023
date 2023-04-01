47432915cfa4d656eba685c2e8b1c4e250600ae8
Brianna <brianna.adewinmbi@gmail.com>
2023-04-01 00:16:53 -0700

    Charge taxi tune
    Co-Authored-By: Ayaka Kawano <59751273+AyakaK005@users.noreply.github.com>
    Co-Authored-By: Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
    Co-Authored-By: Alex Duan <40921264+The1AndOnlyAlex@users.noreply.github.com>

--- src/main/java/frc/robot/commands/ChargeAutos.java
+++ src/main/java/frc/robot/commands/ChargeAutos.java
@@ -416,7 +416,7 @@ public class ChargeAutos {
         Trajectory returnToCharge = TrajectoryGenerator.generateTrajectory(
             List.of(
                 new Pose2d(-5, -0.19, Rotation2d.fromDegrees(0)),
-                new Pose2d(-2.2, -0.21, Rotation2d.fromDegrees(0))
+                new Pose2d(-2, -0.21, Rotation2d.fromDegrees(0))
             ),
             trajectoryConfig);
         
@@ -476,7 +476,7 @@ public class ChargeAutos {
             // Stop completely (tow the modules)
             runOnce(() -> swerveDrive.setModuleStates(SwerveDriveConstants.towModuleStates), swerveDrive),
             runOnce(() -> swerveDrive.stopModules()),
-            waitSeconds(0.2),
+            // waitSeconds(0.2),
             new TurnToAngle(0, swerveDrive),
             runOnce(() -> swerveDrive.resetOdometry(new Pose2d(-5, -0.2, new Rotation2d()))),
             returnToChargeCommand,
