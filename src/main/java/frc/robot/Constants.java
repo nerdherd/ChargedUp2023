f4bd6240ff451757d5fe18827fc7ac8691fb3505
Kyle Huang <72529534+brokenfloppydisk@users.noreply.github.com>
2023-06-10 11:31:20 -0700

    Clean up constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,137 +25,14 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public final class Constants {
 
   public static class DriveConstants {
-
-    // ============== Gigantor =================
-    // public static final int kRightMasterID = 2;
-    // public static final int kLeftMasterID = 1;
-    // public static final int kRightFollowerID = 3;
-    // public static final int kRightFollower2ID = 4;
-    // public static final int kLeftFollowerID = 19;
-    // public static final int kLeftFollower2ID = 20;
-    // public static final double kAngularP = 0;
-    // public static final double kAngularD = 0;
-    // public static final double kLinearP = 0;
-    // public static final double kLinearD = 0;
-
-    // ============== PrototypeBOT =============
-    public static final int kRightMasterID = 0;
-    public static final int kLeftMasterID = 0;
-    public static final double kAngularP = 0.1;
-    public static final double kAngularD = 0;
-    public static final double kLinearP = 0.1;
-    public static final double kLinearD = 0;
-    public static final int kRightFollowerID = 17;
-    public static final int kLeftFollowerID = 31;
-    public static final int kRightFollower2ID = 1;
-    public static final int kLeftFollower2ID = 18;
-
-    // thomas
-    // public static final int kRightMasterID = 9;
-    // public static final int kLeftMasterID = 20;
-    // public static final int kRightFollowerID = 15;
-    // public static final int kLeftFollowerID = 19;
-
     public static final double kDriveAlpha = 0.11765;
     public static final double kDriveOneMinusAlpha = 0.88235;
-    //public static final int kTicksPerFoot = 11738;
-    public static final int kTicksPerMeter = 111555;
     public static final double kErrorBound = 0;
-
-    public static final double kAutoPower = 0;
-
-    public static final int kPistonForwardID = 0;
-    public static final int kPistonReverseID = 1;
-
-    public static final int kFalconMaxCurrent = 50;
   }
 
   public static class ControllerConstants {
     public static final int kDriverControllerPort = 0;
     public static final int kOperatorControllerPort = 1;
-    public static final double kOperatorJoystickDeadband = 0.05;
-  }
-
-  public static class ClawConstants{
-    public static final int kPistonForwardID = 2;
-    public static final int kPistonReverseID = 3;
-    public static final int kPCMPort = 3;
-
-    public static final int kTopMotorID = 61;
-    public static final int kBottomMotorID = 62;
-    public static final double kIntakePower = -0.3;
-    public static final double kOuttakePower = 0.5;
-    public static final double kIntakeNeutralPower = -0.25;
-    public static final int kMainMotorID = 61;
-  }
-
-  public static class ArmConstants{
-    public static final int kRotatingArmID = 17;
-    public static final int kArmLowerLimit = -220000;
-    public static final int kArmStow = -144652;//-310079;//-144652;//-166987; //73 deg //-361131;//-144321 * 3;//-276670;//-622485;//-630435;//144278;
-    public static final int kArmTalonTach = -550169 / 3;
-    public static final int kArmScore = -75933;//-152194;//-70998;//-81958; //35.83 deg //-177246;//-432700;//-377161 - 7950; //-432700;//-408850;//596443; // 89744;
-    public static final int kArmScoreCubeMid = -394900 / 3;//-371050;//596443; // 89744;
-    public static final int kArmScoreCubeHigh = -406900 / 3;//-383050;//596443; // 89744;
-    public static final int kArmGroundPickup = -22763;//-12484;//-26760;//-12484;//-11002; //4.8 deg //-23794;//-198668;//-238900;//-215050;//-242170;//767054;// 34352;
-    public static final int kArmGroundPickupVision = -19430;//-42020;
-    public static final int kArmSubstation = -108137;//-231474;//-107974;//-124643; //54.49 deg//-269557; //-484470;//-479236;//-522850; //-499000;//-480486; // TODO: MEASURE THIS PWEASE
-    public static final int kArmMotionAcceleration = 100000;//20000;//45000;//800000; //160000;
-    public static final int kArmCruiseVelocity = 16333;//13500;//30000;//18000;//21777;
-    public static final double kArmDeadband = 0.05;
-    public static final double kArmP = 0.1;
-    public static final double kArmI = 0;
-    public static final double kArmD = 0.0;
-    public static final double kArmF = 0.0496;
-    public static final double kStowedFF = 0.11;
-    public static final double kExtendedFF = 0.17;
-    public static final double kDiffFF = kExtendedFF - kStowedFF;
-    public static final int kPistonForwardID = 5;
-    public static final int kPistonReverseID = 4;
-    public static final double kJoystickMultiplier = 1; 
-    public static final double kTicksPerAngle = 1981;//4248;//1981;//1603;//4947;//5931;
-    public static final int kTalonTachTopID = 0;
-    public static final int kTalonTachBottomID = 1;
-
-
-    // public static final double kCruiseVelocity = 0;
-    // public static final double kMotionAcceleration = 0;
-
-  }
-
-  public static class ElevatorConstants{
-    public static final int kElevatorID = 18;
-    public static final int kElevatorStow = 0;
-    public static final int kElevatorScoreMidCube = -85285;//-143634;
-    public static final int kElevatorScoreMid = -54512;//-98123; //-143634;
-    public static final int kElevatorScoreHighCube = -198657;//-240000;
-    public static final int kElevatorScoreHigh = -133333;//-240000;
-    public static final int kElevatorSubstation = -40962;//-73732;//-68100;//-116680; // Height for substation i think
-    public static final int kElevatorMotionAcceleration = 32666;
-    public static final int kElevatorCruiseVelocity = 16333;
-    public static final double kElevatorP = 0.1;
-    public static final double kElevatorI = 0;
-    public static final double kElevatorD = 0;
-    public static final double kElevatorF = 0.062;
-    public static final double kArbitraryFF = 0.0085;
-    public static final double kElevatorDeadband = 0.05;
-    public static final double kJoystickMultiplier = 1;
-    public static final double kTicksPerAngle = 0;
-    public static final int kHallEffectID = 2;
-  }
-
-  public static class VisionConstants{
-    public static final double kCameraHeightMeters = Units.inchesToMeters(33.875);
-    public static final double kTargetHeightMeters = Units.inchesToMeters(75);
-    public static final double kCameraPitchRadians = Units.degreesToRadians(30);
-    public static final double kGoalRangeMeters = Units.inchesToMeters(0);
-    public static final double kAreaConstant = 0;
-  }
-
-  public static final class TankAutoConstants {
-    public static final double kPBalancing = 0.3;
-    public static final double kDBalancing = 0;
-
   }
 
   public static final class ModuleConstants {
@@ -170,7 +47,6 @@ public final class Constants {
     public static final double kAbsoluteTurningTicksPer100MsToRadPerSec = kAbsoluteTurningTicksToRad * 10;
     public static final double kIntegratedTurningTicksPer100MsToRadPerSec = kIntegratedTurningTicksToRad * 10;
 
-    
     public static final double kDriveMotorDeadband = 0.02;
     public static final double kTurnMotorDeadband = 0.05;
 
@@ -178,7 +54,6 @@ public final class Constants {
     public static final double kITurning = 0;
     public static final double kDTurning = 0.02; 
     
-    // TODO: tune PID for drive motor velocity control
     public static final double kPDrive = 0.13;
     public static final double kIDrive = 0;
     public static final double kDDrive = 0;
@@ -186,10 +61,11 @@ public final class Constants {
   } 
 
   public static final class SwerveDriveConstants {
-    public static final double kTrackWidth = Units.inchesToMeters(21);      // verify
     // Distance between right and left wheels
-    public static final double kWheelBase = Units.inchesToMeters(21);       // verify
+    public static final double kTrackWidth = Units.inchesToMeters(21);
     // Distance between front and back wheels
+    public static final double kWheelBase = Units.inchesToMeters(21);
+
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
       new Translation2d(kWheelBase / 2, -kTrackWidth / 2), // TODO: This should be front right not front left
       new Translation2d(kWheelBase / 2, kTrackWidth / 2),
@@ -241,11 +117,6 @@ public final class Constants {
       public static final boolean kBLAbsoluteReversed = false;       
       public static final boolean kBRAbsoluteReversed = false; 
 
-      // public static final double kFRAbsoluteOffsetTicks = 4411;       
-      // public static final double kFLAbsoluteOffsetTicks = 3782;         
-      // public static final double kBLAbsouteOffsetTicks = 3042;          
-      // public static final double kBRAbsoluteOffsetTicks = 2364;
-  
       public static final double kFRAbsoluteOffsetTicks = 2794 + 1024;       
       public static final double kFLAbsoluteOffsetTicks = 2354 + 1024 + 52;         
       public static final double kBLAbsoluteOffsetTicks = 2048 + 1024 + 46;          
@@ -266,12 +137,8 @@ public final class Constants {
       public static final double kFRCANCoderOffsetDegrees = 68.379 + 180;       
       public static final double kFLCANCoderOffsetDegrees = 252.949 - 180;         
       public static final double kBLCANCoderOffsetDegrees = 105.293;          
-      public static final double kBRCANCoderOffsetDegrees = 180.176 - 180; // 1.406
+      public static final double kBRCANCoderOffsetDegrees = 180.176 - 180;
     }
-
-
-
-
 
     public static final double kPhysicalMaxSpeedMetersPerSecond = 5;    
     public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 2 * 2 * Math.PI;
@@ -358,31 +225,4 @@ public final class Constants {
     public static final double kDeadband = 0.05;
     public static final double kRotationDeadband = 0.1;
   }
-
-  public static class BananaConstants {
-    public static final double kPIDControllerP = 0.1;
-    public static final double kPIDControllerD = 0.1;
-  }
-
-  public static final class ConeRunnerConstants {
-    public static final int kPositionID = 62;
-    public static final int kSpeedID = 61;
-
-    public static final double kConeRunnerP = 0;
-    public static final double kConeRunnerI = 0;
-    public static final double kConeRunnerD = 0;
-    public static final double kConeRunnerF = 0;
-    public static final double kConeRunnerArbitraryFF = 0;
-
-    public static final double kInitialAngleDegrees = 90;
-    public static final double kDegreesPerTicks = 360 / 4096;
-
-    public static final double kConeRunnerCruiseVelocity = 100;
-    public static final double kConeRunnerMotionAcceleration = 100;
-  }
-  public static final class PneumaticsConstants {
-    public static final int kPCMPort = 3;
-    public static final int kPressureSensorPort = 2;
-  }
-        
 }
